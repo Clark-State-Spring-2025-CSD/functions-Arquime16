40497825ffe84feea3cf3093dc21e06e2fa6d839
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-04-13 16:24:57 -0500

    Update checkinput.py

--- checkinput.py
+++ checkinput.py
@@ -4,3 +4,18 @@
 #The function will continue to prompt the user to enter an integer until a proper integer is entered.
 #The most direct way of doing this would be using a try block, which has not been covered yet. You will need to research this.
 #Write supporting code to call the function, and then display the number that was entered.
+
+
+
+def myFunctionMandE(message, error):
+    user_num = None
+    while user_num == None:
+        user_input = input(message)
+        try:
+            user_num = int(user_input)
+        except ValueError:
+            print(error)
+    print(user_num)
+
+
+myFunctionMandE('Enter an integer: ', 'That is not a valid integer!')

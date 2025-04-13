f934e1a891d841bccfa28fb46a8ee4cf55edb24f
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-04-13 11:58:07 -0500

    Update addnumbers.py

--- addnumbers.py
+++ addnumbers.py
@@ -1,2 +1,23 @@
 #This is a teacher led demo.
 #Watch this video or follow along in class.
+
+def addTwoNumbers(number1, number2):
+    result = number1 + number2
+    return result
+
+def PrintHelloTen():
+    for _ in range(10):
+        print("Hello")
+
+x = 20
+y = 30
+#This is a teacher led demo.
+
+result = addTwoNumbers(x, y)
+
+print(result)
+
+PrintHelloTen()
+
+print("The sum of", x, "and", y, "is", result)
+#This is a teacher led demo.
